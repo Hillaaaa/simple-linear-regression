ef9a262fac97643a2d401ebd0ad72ce4e39883e8
HILLAAA <ocholahillary430@gmail.com>
2026-09-17 23:30:31 +0300

    visualizing the relationship between hours_studied and exam_score using a scatter plot

--- src/explore_data.py
+++ src/explore_data.py
@@ -1,5 +1,7 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 df = pd.read_csv("data/study_hours.csv")
+
 print(df)
 print(df.head())
 print(df.shape)
@@ -9,3 +11,10 @@ print(df.describe())
 print(df.isnull().sum())
 print(df["exam_score"])
 print(df[df["hours_studied"] > 5])
+
+plt.scatter(df['hours_studied'],df["exam_score"])
+plt.xlabel("Hours Studied")
+plt.ylabel("Exam Score")
+plt.title("Hours Studied vs Exam Score")
+plt.show()
+
