557abafff53471e78fd85ffb479214927ec85050
HILLAAA <ocholahillary430@gmail.com>
2026-09-24 11:02:04 +0300

    checked various metrics for my prediction model

--- src/train_test_regression.py
+++ src/train_test_regression.py
@@ -1,6 +1,8 @@
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+import numpy as np
 
 df = pd.read_csv("data/study_hours.csv")
 
@@ -17,3 +19,13 @@ model.fit(X_train, y_train)
 predictions = model.predict(X_test)
 print( predictions)
 print(y_test)
+
+mae = mean_absolute_error(y_test, predictions)
+mse = mean_squared_error(y_test, predictions)
+rmse = np.sqrt(mse)
+r2 = r2_score(y_test, predictions)
+
+print(f"MAE: {mae}")
+print(f"MSE: {mse}")
+print(f"RMSE: {rmse}")
+print(f"R2: {r2}")
