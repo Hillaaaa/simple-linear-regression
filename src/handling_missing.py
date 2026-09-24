921dd3fead5676790ffd9cef6799e530c9d92935
HILLAAA <ocholahillary430@gmail.com>
2026-09-24 18:38:15 +0300

    simple linear regression model on the housing.csv

--- src/handling_missing.py
+++ src/handling_missing.py
@@ -1,3 +1,7 @@
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+import numpy as np
 import pandas as pd
 
 df = pd.read_csv("data/housing.csv")
@@ -12,3 +16,29 @@ df["total_bedrooms"] = df["total_bedrooms"].fillna(median_bedrooms)
 
 print(df.isna().sum())
 
+print(df["ocean_proximity"].unique())
+print(df["ocean_proximity"].value_counts())
+
+df = df[["median_income", "median_house_value"]]
+print(df.head())
+print(df.shape)
+
+X = df[["median_income"]]
+y = df["median_house_value"]
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+predictions = model.predict(X_test)
+
+mae = mean_absolute_error(y_test, predictions)
+mse = mean_squared_error(y_test, predictions)
+rmse = np.sqrt(mse)
+r2 = r2_score(y_test,predictions)
+
+print("Slope:", model.coef_)
+print("Intercept:", model.intercept_)
+print("MAE:", mae)
+print("RMSE:", rmse)
+print("R2:", r2)
